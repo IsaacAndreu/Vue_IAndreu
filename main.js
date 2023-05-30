dd702c5635317a40fc905bc30a4c8617d42baca4
IsaacAndreu <isaacandreu@iesebre.com>
2023-05-30 17:06:32 +0200

    L6-END

--- main.js
+++ main.js
@@ -16,13 +16,11 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        // solution
         removeFromCart() {
             if (this.cart >= 1) {
                 this.cart -= 1
             }
         },
-        // solution
         updateImage(variantImage) {
             this.image = variantImage
         }
