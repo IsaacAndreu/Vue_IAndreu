21d059ecd95dbf5da87d1ac6e577584766f1ac7a
IsaacAndreu <isaacandreu@iesebre.com>
2023-05-30 18:27:34 +0200

    L7-END Nova Versió

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
+            cart:0,
+            product: 'Camiseta del F.C Barcelona',
+            brand: 'Nike',
+            image: './assets/images/camisetabarça.jpg',
+            inStock: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'Blue', image: './assets/images/camisetabarça.jpg' },
+                { id: 2235, color: 'Pink', image: './assets/images/barçarosa.jpg' },
+            ]
         }
     },
-    methods: {}
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
 })
